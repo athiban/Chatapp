a1494cbeaf9635596424ad48b1fcdd3df6f3a365
athiban <athi.mcet@gmail.com>
2024-11-26 11:10:41 +0530

    Initial Commit

---
+++ Chatapp/ChatappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatappApp.swift
+//  Chatapp
+//
+//  Created by Athiban Ragunathan on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
